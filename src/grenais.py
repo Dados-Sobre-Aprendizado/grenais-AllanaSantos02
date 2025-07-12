412cf98b7cf4343d8b9804a17a5953d1f764995d
LuanaGrandi <luanagrandi10@gmail.com>
2025-07-12 11:48:53 -0300

    solucao do problema

--- src/grenais.py
+++ src/grenais.py
@@ -1,6 +1,35 @@
 def main():
-    # TODO: implementar
-    pass
+    inter_vitoria = 0
+    gremio_vitoria = 0
+    empate = 0
+    total_grenais = 0
+    opcao = 1
+
+    while opcao == 1:
+        gols_inter, gols_gremio = map(int, input().split())
+        total_grenais += 1
+
+        if gols_inter > gols_gremio:
+            inter_vitoria += 1
+        elif gols_gremio > gols_inter:
+            gremio_vitoria += 1
+        else:
+            empate += 1
+
+        print("Novo grenal (1-sim 2-nao)")
+        opcao = int(input( ))
+
+    print(f"{total_grenais} grenais")
+    print(f"Inter:{inter_vitoria}")
+    print(f"Gremio:{gremio_vitoria}")
+    print(f"Empates:{empate}")
+
+    if inter_vitoria > gremio_vitoria:
+        print("Inter venceu mais")
+    elif gremio_vitoria > inter_vitoria:
+        print("Gremio venceu mais")
+    else:
+        print("Não houve vencedor")
 
 if __name__ == "__main__":
     main()
